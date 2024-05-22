e018ad5d0903d80ba915bbdaed98db6e123a0975
Alex <147496294+A-Hoenig@users.noreply.github.com>
2024-05-22 10:43:31 +0200

    refactor filter to be dynamic and reusable

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -4,7 +4,14 @@ import Filter from "../../ui/Filter";
 function CabinTableOperations() {
   return (
     <TableOperations>
-      <Filter />
+      <Filter
+        filterField={"discount"}
+        options={[
+          { value: "all", label: "All" },
+          { value: "discounted", label: "Discounted" },
+          { value: "no-discount", label: "No-Discount" },
+        ]}
+      />
     </TableOperations>
   );
 }
