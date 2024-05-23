c2201969e55a2f91b5009c35d777b6cd77afc271
Alex <147496294+A-Hoenig@users.noreply.github.com>
2024-05-23 17:09:11 +0200

    make page count number dynamic

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -5,7 +5,8 @@ export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
+      { count: "exact" }
     );
 
   // FILTER
@@ -16,12 +17,12 @@ export async function getBookings({ filter, sortBy }) {
       ascending: sortBy.direction === "asc",
     });
 
-  const { data, error } = await query;
+  const { data, error, count } = await query;
 
   if (error) {
     throw new Error("Bookings not loaded");
   }
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -79,6 +79,8 @@ function Pagination({ count }) {
     setSearchParams(searchParams);
   }
 
+  if (pageCount <= 1) return null;
+
   return (
     <StyledPagination>
       <p>
