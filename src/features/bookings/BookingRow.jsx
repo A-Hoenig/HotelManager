56e34408161c639b5c9f24e6f73bcb3e600c3984
Alex <147496294+A-Hoenig@users.noreply.github.com>
2024-05-26 14:58:39 +0200

    add checkin menu button and redirect

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -4,7 +4,7 @@ import { format, isToday } from "date-fns";
 import Tag from "../../ui/Tag";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-import { HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
@@ -92,6 +92,13 @@ function BookingRow({
             onClick={() => navigate(`/bookings/${bookingId}`)}
           >
             See Details
+          </Menus.Button>
+
+          <Menus.Button
+            icon={<HiArrowDownOnSquare />}
+            onClick={() => navigate(`/checkin/${bookingId}`)}
+          >
+            Check In
           </Menus.Button>
         </Menus.List>
       </Menus.Menu>
