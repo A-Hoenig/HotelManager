538c30f2ac160e051e8c6dc33991c775562f0c64
Alex <147496294+A-Hoenig@users.noreply.github.com>
2024-05-30 13:47:00 +0200

    add login function

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -22,6 +22,14 @@ const Heading = styled.h1`
       font-size: 2rem;
     `}
 
+    ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-weight: 600;
+      font-size: 3rem;
+      text-align: center;
+    `}
+
   line-height: 1.4;
 `;
 
